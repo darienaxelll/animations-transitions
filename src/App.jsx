42fb4f04aed2eb3d9d097ec8a180592fb47bbbd1
Darien Salazar <darien.axell.salazar@gmail.com>
2024-04-25 22:48:54 -0500

    Created multiple Keyframes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="white ball" />
+      <div className="yellow ball" />
+      <div className="container">
+        <div className="ball orange"/>
+      </div>
+    </>
+  );
+}
+
+export default App;
